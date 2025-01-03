6f168adc5a265e6a0e3a4ccb5c19a87eb7ead758
aigle-levant <aiglelevant@gmail.com>
2025-01-03 13:43:36 +0530

    rearranged files, added gallery page

---
+++ assets/main.js
@@ -0,0 +1,26 @@
+//carousel swiper library
+const swiper = new Swiper('.swiper',
+{
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+          
+    navigation:
+    {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
+
+//lightbox
+let lightbox = document.getElementById("lightbox");
+let lightboxImage = document.getElementById("lightbox-img");
+
+function openLightBox()
+{
+
+}
+function closeLightBox()
+{
+    
+}
